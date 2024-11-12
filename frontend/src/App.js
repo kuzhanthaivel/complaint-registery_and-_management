7a2ad6c62c6eafcb9fb52b32a4e1c44f08e74a77
kuzhanthaivel <kuzhanthaivel272@gmail.com>
2024-11-12 20:54:32 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+
+
+        </p>
+         Online complaint registration and management 
+      </header>
+    </div>
+  );
+}
+
+export default App;
